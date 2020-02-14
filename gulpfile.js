441de0e1b5f714fe7e5c290adf7692277ffa05a5
mdmohasin <mohasinctg@gmail.com>
2020-02-14 20:45:39 +0600

    Update for gulp 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,50 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var minify = require('gulp-minify');
-var cleanCss = require('gulp-clean-css');
+const gulp = require('gulp'),
+ concat = require('gulp-concat'),
+ minify = require('gulp-minify'),
+ cleanCss = require('gulp-clean-css');
 
-/* Add Js file url here */
+/* Add Js and css file url here */
+let path = {
+    jsFiles : [
+        'assets/js/jquery.min.js',
+        'assets/js/jquery.validate.min.js',
+        'assets/js/bootstrap-3.3.7.min.js',
+        'assets/js/owl.carousel.min.js',
+        'assets/js/animation.gsap.js'
+    ],
+    cssFiles: [
+        'assets/css/bootstrap-3.3.7.min.css',
+        'assets/css/owl.carousel.css',
+        'assets/css/style-3.0.0.css'
+    ]
+};
+
+
+// ----- Scripts ----- //
+// Concat, minify js
 gulp.task('pack-js', function () {
+
     // return gulp.src(['assets/js/vendor/*.js', 'assets/js/main.js', 'assets/js/module*.js'])
     //     .pipe(concat('bundle.js'))
     //     .pipe(gulp.dest('public/build/js'));
-    return gulp.src(
-        ['assets/js/jquery.min.js', 'assets/js/jquery.validate.min.js', 'assets/js/bootstrap-3.3.7.min.js',
-            'assets/js/owl.carousel.min.js', 'assets/js/animation.gsap.js'
-        ])
+
+    return gulp.src(path.jsFiles)
         .pipe(concat('bundle.js'))
         .pipe(minify())
         .pipe(gulp.dest('public/build/js'));
 });
 
-/* Add css file url here */
+
 gulp.task('pack-css', function () {
     // return gulp.src(['assets/css/main.css', 'assets/css/custom.css'])
     //     .pipe(concat('stylesheet.css'))
     //     .pipe(gulp.dest('public/build/css'));
-    return gulp.src(['assets/css/bootstrap-3.3.7.min.css', 'assets/css/owl.carousel.css',
-        'assets/css/style-3.0.0.css', 'assets/css/owl.theme.css', 'assets/css/ff18.css','assets/css/responsive.css' ])
+
+    return gulp.src(path.cssFiles)
         .pipe(concat('stylesheet.css'))
         .pipe(cleanCss())
         .pipe(gulp.dest('public/build/css'));
 });
 
-gulp.task('default', ['pack-js', 'pack-css']);
+gulp.task('default',  gulp.series('pack-js', 'pack-css'));
 
